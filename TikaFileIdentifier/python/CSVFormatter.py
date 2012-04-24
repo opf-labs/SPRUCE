6151f509ac6a582d541ebc2f8f993cca06a50e0f
Peter May <Peter.May@bl.uk>
2012-04-24 11:19:00 +0100

    Added reference to config file. Check output directory exists and create if not. Check ignore headings list to ignore specific metadata headings in generated CSV file. Removed .txt extension from filename path in CSV file to reflect exact input file

--- TikaFileIdentifier/python/CSVFormatter.py
+++ TikaFileIdentifier/python/CSVFormatter.py
@@ -6,17 +6,23 @@ Created on 17 Apr 2012
 @contact: Peter.May@bl.uk
 '''
 
+import config
+
 import csv
 import json
 import os
 import sys
 from sets import Set
 
+## HEADINGS to ignore - columns/keywords in this set will be ignored
+## helps prevent strange parsing outputs where one file's results split onto multiple rows
+#IGNORE = ['Inter Color Profile', 'Unknown tag (0x02bc)', 'Unknown tag (0x8649)']
+
 # Useful properties
-PROPS = ['Content-Type', 'Application-Name', 'creator', 'producer', 'Author', 'Content-Length', 'Page-Count', 'Revision-Number', 'Creation-Date', 'Last-Modified', 'Last-Save-Date', 'Last-Printed']
+#PROPS = ['Content-Type', 'Application-Name', 'creator', 'producer', 'Author', 'Content-Length', 'Page-Count', 'Revision-Number', 'Creation-Date', 'Last-Modified', 'Last-Save-Date', 'Last-Printed']
 
 # CSV Headings
-HEADINGS = ['Filename', 'isValid']
+HEADINGS = ['Filename', 'Read TikaRunner Output']
 #HEADINGS.extend(PROPS)      # add in the useful properties headings
 
 def __listFilesInDir(directory):
@@ -30,6 +36,10 @@ def __listFilesInDir(directory):
     return fileList
 
 def processdir(origdir, directory, outputfile):
+    """Aggregates all JSON files in the specified directory into one CSV with the specified
+       outputfile name.  origdir specifies the directory prefix to the original input files
+       and is used to provide the path to the original input file (rather than the TikaRunner's
+       output file) in the CSV""" 
     fileList = __listFilesInDir(directory)
     
     # Work out all headings
@@ -51,11 +61,14 @@ def processdir(origdir, directory, outputfile):
     
     HEADINGS.extend(list(prop_headings))
     
+    # check output folder exists
+    outpath = os.path.dirname(outputfile)
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+    
     # now create an output file
     writer = csv.DictWriter(open(outputfile, 'wb'), HEADINGS, extrasaction='ignore')
     writer.writeheader()
-    
-    print len(fileList)
     
     # now re-run through all the files again and fill out each row 
     for file in fileList:
@@ -72,12 +85,17 @@ def processdir(origdir, directory, outputfile):
                 metadata = json.load(fileObj, encoding="cp1252")
             except Exception, e:
                 print file, e 
-                metadata['isValid'] = False
+                metadata['Read TikaRunner Output'] = False
+                
+        # delete any data from keys from the IGNORE list
+        for k in config.IGNORE:
+            if k in metadata:
+                metadata[k] = "Data removed. See JSON output file"
         
         # add in the filename
         relpath     = os.path.dirname(os.path.relpath(file, directory))
         absOutPath  = os.path.join(origdir, relpath, fname)
-        metadata['Filename']=absOutPath
+        metadata['Filename']=absOutPath[:-4]    # removes the added ".txt" also
         
         # write output to file
         writer.writerow(metadata)
